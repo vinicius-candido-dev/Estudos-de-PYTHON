2d546825f39f193a2461a8b6b5703ae6c261a13a
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-06-03 19:26:34 -0300

    idk

--- codes/POO/desafios/des019/ex19.py
+++ codes/POO/desafios/des019/ex19.py
@@ -1,20 +1,38 @@
 from rich import print
 from rich import emoji
+from time import sleep
 class Livro():
     def __init__(self, titulo, paginas):
         self.titulo = titulo
         self.paginas = paginas
-
+        self.pag_atual = 1
         
+        print(f':open_book: [blue]Você acabou de abrir o livro [red]"{self.titulo}"[/] que tem [green] {self.paginas} página[/] no total. Você agora está na [yellow]página {self.pag_atual}[/][/blue]')
 
-    def avamcar_paginas(self, count):
-        pag = 1
-        if pag == 1:
-            return f'[bold blue]Você acabou de abrir o livro "[/][bold red]{self.titulo}[/][bold blue]" que tem[/] [bold green]{self.paginas} paginas[/] [bold blue]no total. Você agora está na [/][bold yellow] página {self.paginas} [/]'
+    def avancar_paginas(self, qtd = 1):
+        cont = 0
+        for a in range(self.pag_atual,qtd, 1):
+                if not self.fim_livro():
+                    self.pag_atual +=1
+                    print( f'Pág{self.pag_atual} :arrow_forward:', end=' ')
+                    sleep(0.4)
+                    cont +=1
+        print(f"[blue]Você avançou {cont} páginas e agora está na [yellow]página {self.pag_atual}[/][/blue]')")
+        if self.fim_livro():
+            print(f':closed_book: [red]Você chegou ao final do filme "{self.titulo}"[/]')
+    def fim_livro(self) -> bool:
+        return True if self.pag_atual == self.paginas else False
+
+        '''if self.paginas == self.pag_atual:
+            return True
         else:
-            pass
+            return False'''
 
 
-d1 = Livro('Em busca da verdade',20)
-print(d1)
-#print(d1.avamcar_paginas(5))
+
+d1 = Livro('Em busca da verdade', 20)
+d1.avancar_paginas(5)
+d1.avancar_paginas(10)
+d1.avancar_paginas(50)
+
+#print(d1.avancar_paginas(5))
--- codes/POO/desafios/des020/ex20.py
+++ codes/POO/desafios/des020/ex20.py
@@ -3,24 +3,27 @@ from rich.panel import Panel
 class Gamer:
     def __init__(self, nome, nick):
         self.nome = nome 
-        self.nick = nick 
+        self.nick = nick
+        self.all = list()
         
     def add_favorite(self, add):
-        lista = []
-        lista.append(add)
-        
-
-
-
+        #self.all += f'\n:video_game: [blue]{add}[/blue]'
+        self.all.append(add)
+        self.all = sorted(self.all, key=str.lower)
 
 
     def ficha(self):
-        jog = Panel(f'Nome real: [reverse blue] {self.nome} [/]' '\nJogos favoritos:',
-                    title=f"jogador <{self.nick}>",width=50, height=10)
-
+        conteudo = f'Nome real: [reverse blue] {self.nome}[/]'
+        conteudo += f'\nJogos favoritos:'
+        for num, game in enumerate(self.all):
+            conteudo += f'\n:video_game: [blue]{game}[/]'
+        jog = Panel(conteudo, title=f"jogador <{self.nick}>",width=50)
         return jog
 
 
-
 g1 = Gamer("Vinicius Cândido","vini")
+g1.add_favorite('Silksong')
+g1.add_favorite('God of War')
+g1.add_favorite('Chess')
+g1.add_favorite('Dark souls')
 print(g1.ficha())
