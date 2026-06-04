a37015da0097dacc8e9cf129e8d0f1351cc528df
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-06-03 22:52:49 -0300

    Update ex21.py

--- codes/POO/desafios/des021/ex21.py
+++ codes/POO/desafios/des021/ex21.py
@@ -1,3 +1,34 @@
+from rich import print
 class Caneta:
-    def __init__(self):
-        
+    def __init__(self, cor = "azul"):
+        match cor.lower().strip():
+            case "azul":
+                escolha = "[blue]"
+            case  "vermelho":
+                escolha = "[red]"
+            case "verde":
+                escolha = "[green]"
+            case _:
+                escolha = "[white]"
+        self.cor =  escolha
+
+    def queb_linh(self, qtd = 1):
+        return'\n' * qtd
+
+    def escrever(self, msg):
+        return f'{self.cor}{msg}[/]'
+
+    def tampar(self):
+        pass
+
+    def destampar(self):
+        pass
+
+
+
+
+c1 = Caneta("azul")
+c2 = Caneta('vermelha')
+c3 = Caneta()
+print(c1.escrever('Olá, mundo'))
+
